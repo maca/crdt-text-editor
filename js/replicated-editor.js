8835497deaaf862c786d898e9d6ed6a4c4bbf62f
Macario <maca@aelita.io>
2019-06-14 23:57:39 +0200

    selection is keept

--- js/replicated-editor.js
+++ js/replicated-editor.js
@@ -2,31 +2,43 @@ class ReplicatedEditor extends HTMLElement {
   constructor() {
     super();
     this.emitContentChanged = true;
-    this.bufferOperations = false;
-    this.operationsBuffer = [];
   }
 
   set content(nodes) {
     this.nodes = nodes;
-    if (this.editor) { this.sync(); }
+
+    if (!this.editor) { return }
+
+    try {
+      this.emitContentChanged = false;
+      this.session.doc.setValue(this.text);
+    } finally {
+      this.emitContentChanged = true;
+    }
+  }
+
+  set selection({start, end, reverse}) {
+    if (!this.editor) { return }
+
+    const rangeStart = this.session.doc.indexToPosition(start)
+      , rangeEnd = this.session.doc.indexToPosition(end)
+      , selection = { start: rangeStart, end: rangeEnd }
+
+    this.editor.selection.setSelectionRange(selection, reverse)
   }
 
   get text() {
-    return this.visibleNodes
-      .map(({value}) => { return value }).join('');
-  }
-
-  get visibleNodes() {
-    return this.nodes
-      .filter(({isDeleted}) => { return !isDeleted });
+    return this
+      .nodes.map(({value}) => { return value }).join('');
   }
 
   get session() {
     return this.editor.session;
   }
 
-  nodeAt(idx) {
-    return this.visibleNodes[idx];
+  dispatch(name, detail) {
+    const ev = new CustomEvent(name, { detail: detail });
+    this.dispatchEvent(ev);
   }
 
   contentChanged({ action, start, end, lines }) {
@@ -42,22 +54,20 @@ class ReplicatedEditor extends HTMLElement {
       ops = this.contentRemoved(from, changes)
     }
 
-    this.dispatchEvent(
-      new CustomEvent('editorChanged', { detail: ops })
-    );
-
-    this.sync()
+    this.dispatch('editorChanged', ops);
   }
 
   contentInserted(from, changes) {
-    const prevNode = this.visibleNodes[from - 1]
+    const prevNode = this.nodes[from - 1]
       , addFun = chr => { return { op: "add", value: chr } }
 
     if (prevNode) {
-      const first = changes.shift()
-        , ops = changes.map(addFun)
+      const first = changes.shift(), ops = changes.map(addFun);
 
-      ops.unshift({ op: "addAfter", value: first, id: prevNode.id })
+      ops.unshift({ op: "addAfter"
+        , value: first, path: prevNode.path
+      })
+
       return ops;
     } else {
       return changes.map(addFun);
@@ -66,70 +76,32 @@ class ReplicatedEditor extends HTMLElement {
 
   contentRemoved(from, changes) {
     const to = from + changes.length
-      , slice = this.visibleNodes.slice(from, to)
+      , slice = this.nodes.slice(from, to)
+      , deleteOp = ({path}) => { return { op: 'delete', path: path } }
 
-    return slice.map(({id}) => {
-      return { op: 'delete', id: id }
-    });
+    return slice.map(deleteOp);
   }
 
   cursorChanged() {
     if (!this.emitContentChanged) { return }
 
     const { start, end } = this.editor.selection.getRange()
-      , doc = new this.session.doc.constructor(this.text)
-      , startIndex = doc.positionToIndex(start)
-      , endIndex = doc.positionToIndex(end)
-      , startNode = this.nodeAt(startIndex)
-      , endNode = this.nodeAt(endIndex)
+      , startIndex = this.session.doc.positionToIndex(start)
+      , endIndex = this.session.doc.positionToIndex(end)
+      , reverse = this.editor.selection.isBackwards()
+      , selection = { start: startIndex, end: endIndex, reverse: reverse }
 
-      this.selection =
-        { startNode: startNode, endNode: endNode }
-  }
-
-  indexOrPrev(node) {
-    let idx = this.nodes.indexOf(node);
-
-    while (idx >= 0 && node.removed) {
-      idx = idx - 1;
-      node = this.nodes[idx];
-    }
-
-    return this.visibleNodes.indexOf(node);
-  }
-
-  sync() {
-    this.emitContentChanged = false
-
-    try {
-      const doc = new this.session.doc.constructor(this.text)
-        , { startNode, endNode } = this.selection
-        , startIndex = this.indexOrPrev(startNode)
-        , endIndex  = this.indexOrPrev(endNode)
-        , rangeStart = doc.indexToPosition(startIndex)
-        , rangeEnd = doc.indexToPosition(endIndex)
-        , range = { start: rangeStart, end: rangeEnd }
-
-      this.session.doc.setValue(this.text)
-      this.editor.selection.setSelectionRange(range)
-    } finally {
-      this.emitContentChanged = true
-    }
+    this.dispatch('selectionChanged', selection);
   }
 
   connectedCallback () {
     const contentChanged = this.contentChanged.bind(this)
       , cursorChanged = this.cursorChanged.bind(this)
-      , lastNode = this.visibleNodes[this.visibleNodes.length - 1]
 
     this.editor = ace.edit(this, {});
     this.editor.$blockScrolling = Infinity;
-    this.selection = { startNode: lastNode, endNode: lastNode }
-
     this.session.on('change', contentChanged);
     this.editor.selection.on('changeCursor', cursorChanged);
-
-    this.sync()
   }
 };
 
--- src/Main.elm
+++ src/Main.elm
@@ -2,42 +2,54 @@ import Browser
 import Browser.Dom as Dom
 import CRDTree exposing
   (CRDTree, Error, add, addAfter, delete, batch)
-import CRDTree.Node as Node exposing (Node)
+import CRDTree.Node as Node exposing (Node, isDeleted)
 import Html exposing (..)
 import Html.Attributes exposing (attribute, style, id, property)
 import Html.Events exposing (on)
+import Html.Keyed
+import Html.Lazy exposing (lazy)
 import Json.Decode as Decode exposing
   (Decoder, at, field, decodeValue)
 import Json.Encode as Encode
 
 
 type alias Model =
-  { tree : CRDTree Char }
+  { tree : CRDTree Char
+  , selection: Selection
+  }
 
 
-type Msg
-  = EditorChanged (List OperationFunction)
+type alias Selection =
+  { start: Int, end: Int, reverse: Bool }
 
 
 type alias OperationFunction =
   CRDTree Char -> Result (Error Char) (CRDTree Char)
 
 
+type Msg
+  = EditorChanged (List OperationFunction)
+  | SelectionChanged Selection
+
+
 main : Program Decode.Value Model Msg
 main =
   Browser.document
     { init = init
-    , view = \model ->
-        { title = "Editor", body = [view model] }
+    , view = \model -> { title = "Editor", body = [view model] }
     , update = update
     , subscriptions = always Sub.none
     }
 
 
 init flags =
-  ( { tree = CRDTree.init { id = 0, maxReplicas = 1024 } }
-  , Cmd.none
-  )
+  let
+      model =
+        { tree = CRDTree.init { id = 0, maxReplicas = 1 }
+        , selection = Selection 0 0 False
+        }
+  in
+  ( model, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -45,26 +57,47 @@ update msg model =
   case msg of
     EditorChanged funcs ->
       case batch funcs model.tree of
-        Ok tree ->
-          ( { model | tree = tree }, Cmd.none )
+        Ok (tree) ->
+          let
+              selection = collapseSelection model.selection
+          in
+          ( { model | tree = tree, selection = selection }
+          , Cmd.none
+          )
+
         Err _ ->
           ( model, Cmd.none )
+
+    SelectionChanged selection ->
+      ( { model | selection = selection }, Cmd.none )
+
+
+collapseSelection : Selection -> Selection
+collapseSelection {start} =
+  Selection start start False
 
 
 view : Model -> Html Msg
 view model =
+  viewEditor model
+
+
+viewEditor : Model -> Html Msg
+viewEditor model =
   Html.node "replicated-editor"
     [ id "crdt-editor"
     , style "width" "100vw"
     , style "height" "100vh"
     , property "content" <| contentEncoder model.tree
-    , on "editorChanged" decodeEditorChanged
+    , property "selection" <| selectionEncoder model.selection
+    , on "editorChanged" editorChangeDecoder
+    , on "selectionChanged" <| selectionChangeDecoder model.tree
     ]
     [ ]
 
 
-decodeEditorChanged : Decoder Msg
-decodeEditorChanged =
+editorChangeDecoder : Decoder Msg
+editorChangeDecoder =
   Decode.map EditorChanged
     (field "detail" (Decode.list editorOperationDecoder))
 
@@ -84,12 +117,12 @@ editorOperationDecoderHelp opType =
 
     "addAfter" ->
       Decode.map2 addAfter
-        (field "id" <| Decode.list Decode.int)
+        (field "path" <| Decode.list Decode.int)
         (field "value" charDecoder)
 
     "delete" ->
       Decode.map delete
-        (field "id" <| Decode.list Decode.int)
+        (field "path" <| Decode.list Decode.int)
 
     _ ->
       Decode.fail "Unknown operation"
@@ -112,6 +145,7 @@ contentEncoder : CRDTree Char -> Encode.Value
 contentEncoder tree =
   CRDTree.root tree
     |> Node.children
+    |> List.filter (\node -> not (isDeleted node))
     |> List.reverse
     |> Encode.list nodeEncoder
 
@@ -127,8 +161,32 @@ nodeEncoder node =
   in
   Encode.object
     [ ( "value", value )
-    , ( "id", Node.path node |> Encode.list Encode.int )
+    , ( "path", Node.path node |> Encode.list Encode.int )
+    , ( "timestamp", Node.timestamp node |> Encode.int )
     , ( "isDeleted", Node.isDeleted node |> Encode.bool )
     ]
+
+
+selectionEncoder : Selection -> Encode.Value
+selectionEncoder {start, end, reverse} =
+  Encode.object
+    [ ( "start", Encode.int start )
+    , ( "end", Encode.int end )
+    , ( "reverse", Encode.bool reverse )
+    ]
+
+
+selectionDecoder : (CRDTree Char) -> Decoder Selection
+selectionDecoder tree =
+  Decode.map3 Selection
+    (field "start" Decode.int)
+    (field "end" Decode.int)
+    (field "reverse" Decode.bool)
+
+
+selectionChangeDecoder : (CRDTree Char) -> Decoder Msg
+selectionChangeDecoder tree =
+  Decode.map SelectionChanged
+    (field "detail" (selectionDecoder tree))
 
 
