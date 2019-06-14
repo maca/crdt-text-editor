133b36b7fe88eb0babf76a9b5e0dd126cc010262
Macario <maca@aelita.io>
2019-06-14 22:56:30 +0200

    add and delete

--- js/replicated-editor.js
+++ js/replicated-editor.js
@@ -1,8 +1,135 @@
 class ReplicatedEditor extends HTMLElement {
-  constructor() { super(); }
+  constructor() {
+    super();
+    this.emitContentChanged = true;
+    this.bufferOperations = false;
+    this.operationsBuffer = [];
+  }
+
+  set content(nodes) {
+    this.nodes = nodes;
+    if (this.editor) { this.sync(); }
+  }
+
+  get text() {
+    return this.visibleNodes
+      .map(({value}) => { return value }).join('');
+  }
+
+  get visibleNodes() {
+    return this.nodes
+      .filter(({isDeleted}) => { return !isDeleted });
+  }
+
+  get session() {
+    return this.editor.session;
+  }
+
+  nodeAt(idx) {
+    return this.visibleNodes[idx];
+  }
+
+  contentChanged({ action, start, end, lines }) {
+    if (!this.emitContentChanged) { return }
+    let ops;
+
+    const from = this.session.doc.positionToIndex(start)
+      , changes = lines.join("\n").split('')
+
+    if (action === 'insert') {
+      ops = this.contentInserted(from, changes)
+    } else if (action === 'remove') {
+      ops = this.contentRemoved(from, changes)
+    }
+
+    this.dispatchEvent(
+      new CustomEvent('editorChanged', { detail: ops })
+    );
+
+    this.sync()
+  }
+
+  contentInserted(from, changes) {
+    const prevNode = this.visibleNodes[from - 1]
+      , addFun = chr => { return { op: "add", value: chr } }
+
+    if (prevNode) {
+      const first = changes.shift()
+        , ops = changes.map(addFun)
+
+      ops.unshift({ op: "addAfter", value: first, id: prevNode.id })
+      return ops;
+    } else {
+      return changes.map(addFun);
+    }
+  }
+
+  contentRemoved(from, changes) {
+    const to = from + changes.length
+      , slice = this.visibleNodes.slice(from, to)
+
+    return slice.map(({id}) => {
+      return { op: 'delete', id: id }
+    });
+  }
+
+  cursorChanged() {
+    if (!this.emitContentChanged) { return }
+
+    const { start, end } = this.editor.selection.getRange()
+      , doc = new this.session.doc.constructor(this.text)
+      , startIndex = doc.positionToIndex(start)
+      , endIndex = doc.positionToIndex(end)
+      , startNode = this.nodeAt(startIndex)
+      , endNode = this.nodeAt(endIndex)
+
+      this.selection =
+        { startNode: startNode, endNode: endNode }
+  }
+
+  indexOrPrev(node) {
+    let idx = this.nodes.indexOf(node);
+
+    while (idx >= 0 && node.removed) {
+      idx = idx - 1;
+      node = this.nodes[idx];
+    }
+
+    return this.visibleNodes.indexOf(node);
+  }
+
+  sync() {
+    this.emitContentChanged = false
+
+    try {
+      const doc = new this.session.doc.constructor(this.text)
+        , { startNode, endNode } = this.selection
+        , startIndex = this.indexOrPrev(startNode)
+        , endIndex  = this.indexOrPrev(endNode)
+        , rangeStart = doc.indexToPosition(startIndex)
+        , rangeEnd = doc.indexToPosition(endIndex)
+        , range = { start: rangeStart, end: rangeEnd }
+
+      this.session.doc.setValue(this.text)
+      this.editor.selection.setSelectionRange(range)
+    } finally {
+      this.emitContentChanged = true
+    }
+  }
 
   connectedCallback () {
-    this._editor = ace.edit(this, {})
+    const contentChanged = this.contentChanged.bind(this)
+      , cursorChanged = this.cursorChanged.bind(this)
+      , lastNode = this.visibleNodes[this.visibleNodes.length - 1]
+
+    this.editor = ace.edit(this, {});
+    this.editor.$blockScrolling = Infinity;
+    this.selection = { startNode: lastNode, endNode: lastNode }
+
+    this.session.on('change', contentChanged);
+    this.editor.selection.on('changeCursor', cursorChanged);
+
+    this.sync()
   }
 };
 
--- src/Main.elm
+++ src/Main.elm
@@ -1,47 +1,54 @@
 import Browser
 import Browser.Dom as Dom
-import CRDTree exposing (CRDTree, add)
+import CRDTree exposing
+  (CRDTree, Error, add, addAfter, delete, batch)
+import CRDTree.Node as Node exposing (Node)
 import Html exposing (..)
 import Html.Attributes exposing (attribute, style, id, property)
-import Json.Decode as Decode exposing (Decoder, decodeValue)
+import Html.Events exposing (on)
+import Json.Decode as Decode exposing
+  (Decoder, at, field, decodeValue)
+import Json.Encode as Encode
 
 
 type alias Model =
   { tree : CRDTree Char }
 
 
-type Msg = Msg
+type Msg
+  = EditorChanged (List OperationFunction)
+
+
+type alias OperationFunction =
+  CRDTree Char -> Result (Error Char) (CRDTree Char)
 
 
 main : Program Decode.Value Model Msg
 main =
   Browser.document
     { init = init
-    , view = \model -> { title = "Editor", body = [view model] }
+    , view = \model ->
+        { title = "Editor", body = [view model] }
     , update = update
     , subscriptions = always Sub.none
     }
 
 
 init flags =
-  let
-      tree =
-        CRDTree.init { id = 0, maxReplicas = 1024 }
-
-      tree2 =
-        add 'a' tree
-          |> Result.andThen (add 'b')
-          |> Result.andThen (add 'c')
-          |> Result.andThen (add 'd')
-          |> Result.andThen (add 'b')
-          |> Result.withDefault tree
-  in
-  ( { tree = tree2 }, Cmd.none )
+  ( { tree = CRDTree.init { id = 0, maxReplicas = 1024 } }
+  , Cmd.none
+  )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  ( model, Cmd.none )
+  case msg of
+    EditorChanged funcs ->
+      case batch funcs model.tree of
+        Ok tree ->
+          ( { model | tree = tree }, Cmd.none )
+        Err _ ->
+          ( model, Cmd.none )
 
 
 view : Model -> Html Msg
@@ -50,7 +57,78 @@ view model =
     [ id "crdt-editor"
     , style "width" "100vw"
     , style "height" "100vh"
+    , property "content" <| contentEncoder model.tree
+    , on "editorChanged" decodeEditorChanged
     ]
     [ ]
+
+
+decodeEditorChanged : Decoder Msg
+decodeEditorChanged =
+  Decode.map EditorChanged
+    (field "detail" (Decode.list editorOperationDecoder))
+
+
+editorOperationDecoder : Decoder OperationFunction
+editorOperationDecoder =
+  (field "op" Decode.string)
+    |> Decode.andThen editorOperationDecoderHelp
+
+
+editorOperationDecoderHelp : String -> Decoder OperationFunction
+editorOperationDecoderHelp opType =
+  case opType of
+    "add" ->
+      Decode.map add
+        (field "value" charDecoder)
+
+    "addAfter" ->
+      Decode.map2 addAfter
+        (field "id" <| Decode.list Decode.int)
+        (field "value" charDecoder)
+
+    "delete" ->
+      Decode.map delete
+        (field "id" <| Decode.list Decode.int)
+
+    _ ->
+      Decode.fail "Unknown operation"
+
+
+charDecoder : Decoder Char
+charDecoder =
+  Decode.string |> Decode.andThen charDecoderHelp
+
+
+charDecoderHelp : String -> Decoder Char
+charDecoderHelp string =
+  String.uncons string
+    |> Maybe.map (Tuple.first)
+    |> Maybe.map Decode.succeed
+    |> Maybe.withDefault (Decode.fail "Empty string")
+
+
+contentEncoder : CRDTree Char -> Encode.Value
+contentEncoder tree =
+  CRDTree.root tree
+    |> Node.children
+    |> List.reverse
+    |> Encode.list nodeEncoder
+
+
+nodeEncoder : Node Char -> Encode.Value
+nodeEncoder node =
+  let
+      value =
+        Node.value node
+          |> Maybe.map String.fromChar
+          |> Maybe.withDefault ""
+          |> Encode.string
+  in
+  Encode.object
+    [ ( "value", value )
+    , ( "id", Node.path node |> Encode.list Encode.int )
+    , ( "isDeleted", Node.isDeleted node |> Encode.bool )
+    ]
 
 
